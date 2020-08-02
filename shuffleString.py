2145afc5a057834b77625c6379f536b8e693867d
Akash <krishnanag1996@gmail.com>
2020-08-02 23:57:44 +0530

    erqwe

--- shuffleString.py
+++
@@ -1,7 +0,0 @@
-def restoreString(string, indices):
-    ans = [0 for _ in string]
-    for index, char in enumerate(string):
-        ans[indices[index]] = char
-    return "".join(ans)
-
-print(restoreString(string, indices))
